560174f7d5877cda0704b95d418b9befd96815ad
Aleksa Djekic <leki.djekic@gmail.com>
2025-07-12 20:00:00 +0200

    Navbar completed[C

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Header from './components/Header'
 import './App.css'
+import Navbar from './components/Navbar'
+import {useLocation} from 'react-router-dom'
 
 function App() {
+
+  // ako je path/owner skalnja se navbar
+  const isOwner=useLocation().pathname.includes("owner");
 
   return (
     <>
      <Header/>
+     {!isOwner && <Navbar/>}
     </>
   )
 }
