b90d94c227f63f6c5e075c8ef055de852d83cefd
Aleksa Djekic <leki.djekic@gmail.com>
2025-07-12 18:22:23 +0200

    Initialized repository

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div id='Header'>
+      <img className='bg-cover bg-center flex mb-10 w-full 
+      overflow-hidden' src="./assets/heroImage.png" alt="" />
+    </div>
+  )
+}
+
+export default Header
