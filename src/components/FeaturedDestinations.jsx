491a180696a085ee0cb9916cdab32f147cc1bb86
Aleksa Djekic <leki.djekic@gmail.com>
2025-07-15 21:12:06 +0200

    Title and subTitle for cards added

--- src/components/FeaturedDestinations.jsx
+++ src/components/FeaturedDestinations.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import HotelCard from './HotelCard'
 import { roomsDummyData } from '../../assets/assets'
+import Title from './Title'
 
 //mountovanje kartice za hotel 
 const FeaturedDestinations = () => {
   return (
     <div className='flex flex-col items-center px-6 md:px-16 lg:px-24 bg-slate-50 py-20'>
+      <Title title='Featured Destinations' subTitle='
+      Discover our handpicked selection of exceptional properties around the world, 
+      offering unparalleled luxury and experiences'/>
       <div className='flex flex-wrap items-center justify-between gap-6 mt-20'>
         {roomsDummyData.slice(0,4).map((room,index)=>(
             <HotelCard key={room._id} room={room} index={index}/>
