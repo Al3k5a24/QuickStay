6c7e1ee71a75a38214a2b99384c73aadea6456b6
Aleksa Djekic <leki.djekic@gmail.com>
2025-07-15 19:51:57 +0200

    Header section completed!

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { useUser, useClerk, UserButton } from '@clerk/clerk-react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
+// ikonica kad se korisnik uloguje, umesto login buttona
 const BookIcon = ()=>(
     <svg className="w-4 h-4 text-gray-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" >
     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 19V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v13H7a2 2 0 0 0-2 2Zm0 0a2 2 0 0 0 2 2h12M9 3v14m7 0v4" />
@@ -26,12 +29,21 @@ const Navbar = () => {
     const location = useLocation();
 
     React.useEffect(() => {
+
+        if(location.pathname !== '/') {
+            setIsScrolled(true);
+            return;
+        }else{
+            setIsScrolled(false);
+        }
+        setIsScrolled(prev => location.pathname !== '/' ? true : prev)
+        
         const handleScroll = () => {
             setIsScrolled(window.scrollY > 10);
         };
         window.addEventListener("scroll", handleScroll);
         return () => window.removeEventListener("scroll", handleScroll);
-    }, []);
+    }, [location.pathname]);
 
     return (
             <nav className={`fixed top-0 left-0 bg-transparent w-full flex items-center justify-between px-4 md:px-16 lg:px-24 xl:px-32 transition-all duration-500 z-50 ${isScrolled ? "bg-white/80 shadow-md text-gray-700 backdrop-blur-lg py-3 md:py-4" : "py-4 md:py-6"}`}>
@@ -49,6 +61,12 @@ const Navbar = () => {
                             <div className={`${isScrolled ? "bg-gray-700" : "bg-white"} h-0.5 w-0 group-hover:w-full transition-all duration-300`} />
                         </a>
                     ))}
+
+                    {user && <button className={`border px-4 py-1 text-sm font-light 
+                    rounded-full cursor-pointe transition-all ${isScrolled ? 'text-black' : 'text-white'}`} onClick={() => navigate('/owner')}>
+                        Dashboard
+                    </button>}
+
                 </div>
 
                 {/* Desktop Right */}
@@ -73,11 +91,7 @@ const Navbar = () => {
                 {/* Mobile Menu Button */}
 
                 <div className="flex items-center gap-3 md:hidden">
-                    <svg onClick={() => setIsMenuOpen(!isMenuOpen)} className={`h-6 w-6 cursor-pointer ${isScrolled ? "invert" : ""}`} fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
-                        <line x1="4" y1="6" x2="20" y2="6" />
-                        <line x1="4" y1="12" x2="20" y2="12" />
-                        <line x1="4" y1="18" x2="20" y2="18" />
-                    </svg>
+                    <img src="/assets/menuIcon.svg" className='cursor-pointer'  alt="" onClick={() => setIsMenuOpen(!isMenuOpen)} />
                 </div>
 
                 {/* Mobile Menu */}
@@ -100,6 +114,11 @@ const Navbar = () => {
                             {link.name}
                         </a>
                     ))}
+
+                    {user && <button className='border px-4 py-1 text-sm font-light 
+                    rounded-full cursor-pointer transition-all' onClick={() => navigate('/owner')}>
+                        Dashboard
+                    </button>}
 
                     {!user && <button onClick={openSignIn} className="bg-black text-white px-8 py-2.5 rounded-full transition-all duration-500">
                         Login
