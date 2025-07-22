a490536851d55790429e3c417f3ec980f9502ef4
Aleksa Djekic <leki.djekic@gmail.com>
2025-07-22 22:28:17 +0200

    EO component finished

--- src/components/ExclusiveOffers.jsx
+++ src/components/ExclusiveOffers.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Title from './Title'
-import { assets } from '../../assets/assets'
+import { assets, exclusiveOffers } from '../../assets/assets'
 
 const ExclusiveOffers = () => {
   return (
@@ -18,8 +18,30 @@ const ExclusiveOffers = () => {
         </button>
       </div>
 
-      <div>
-
+      {/*Kartice za Exclusive Offers*/} 
+      <div className='mt-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full'>
+        {exclusiveOffers.map((item) => (
+            <div key={item._id} className='rounded-2xl group relative flex flex-col
+            items-start justify-between gap-1 pt-12 md:pt-18 px-4
+            rounded-x1 text-white bg-no-repeat bg-cover bg-center'
+            style={{backgroundImage: `url(${item.image})`}}>
+                <p className='px-3 py-1 absolute top-4 left-4
+                text-xs bg-white text-gray-800 font-medium rounded-full'>{item.priceOff}% OFF</p>
+                <div>
+                    <p className='text-2x1
+                     font-medium font-playfair'>{item.title}</p>
+                    <p>{item.description}</p>
+                    <p className='text-xs
+                     text-white/70 mt-3'>Expires {item.expiryDate}</p>
+                </div>
+                <button className='flex items-center gap-2 
+        font-medium cursor-pointer mt-4 mb-5'>
+            View Offer
+            <img src={assets.arrowIcon} alt="arrow-icon" 
+            className='invert group-hover:translate-x-1 transition-all'/>
+        </button>
+            </div>
+        ))}
       </div>
 
     </div>
