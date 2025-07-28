1f1ef1f5a0bee9d4712dfb158dfa8aeed1d226a8
Aleksa Djekic <leki.djekic@gmail.com>
2025-07-28 23:13:35 +0200

    Add Room page started

--- src/pages/HotelOwner/AddRoom.jsx
+++ src/pages/HotelOwner/AddRoom.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Title from '../../components/Title'
 
 const AddRoom = () => {
+
+  const[images,setImages]=useState({
+    1:null,
+    2:null,
+    3:null,
+    4:null
+  })
+
+  const [inputs, setInputs]=useState({
+    roomType: '',
+    pricePerNight: 0,
+    amenities:{
+      'Free WiFi': false,
+      'Free Breakfast': false,
+      'Free Service': false,
+      'Mountain View': false,
+      'Poll Access': false
+    }
+  })
+
+
   return (
-    <div>
-    </div>
+    <form>
+      <Title align='left' font='outfit' title='Add Room' subTitle='
+      Fill in the details carefully and accurate room details, pricing, and amenities, 
+      to enhance the user booking experioence'/>
+    </form>
   )
 }
 
